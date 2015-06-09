a595bedcc8ed51f82caa94b91dbeecbddfafc22a
luxkyluke <rock_on77500@hotmail.fr>
2015-06-09 16:06:15 +0200

    nouvelle actvité

--- src/com/example/testsnap/MessagerieActivity.java
+++ src/com/example/testsnap/MessagerieActivity.java
@@ -1,41 +1,23 @@
 package com.example.testsnap;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
-public class LoginActivity extends Activity {
+public class MessagerieActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+		setContentView(R.layout.activity_messagerie);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
+		getMenuInflater().inflate(R.menu.messagerie, menu);
 		return true;
-	}
-	
-	public void login(View v){
-		EditText login;
-		login = (EditText) findViewById (R.id.identifiant);
-		String log = login.getText().toString();
-		EditText pass;
-		pass = (EditText) findViewById (R.id.passwrd);
-		String passd = pass.getText().toString();
-		Intent intent = new Intent(this, MessagerieActivity.class);
-		
-		Toast.makeText(getApplicationContext(), (String) log, Toast.LENGTH_SHORT).show();
-		Toast.makeText(getApplicationContext(), (String) passd, Toast.LENGTH_SHORT).show();
-		startActivity(intent);
 	}
 
 	@Override
